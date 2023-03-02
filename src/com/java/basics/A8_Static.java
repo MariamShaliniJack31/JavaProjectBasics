006a68dc842b3316a872c01bec1a11fdf2878f7a
MariamShaliniJack31 <mariamshalini@gmail.com>
2023-03-01 19:03:19 -0500

    2nd Commit on 01-Mar-23

--- src/com/java/basics/A8_Static.java
+++ src/com/java/basics/A8_Static.java
@@ -5,9 +5,10 @@ public class A8_Static {
 	static int cube(int x){  
 		return x*x*x;
 	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int result = A8_Static.cube(5);  
+		int result = A8_Static.cube(5);  /////////////Here we called static method cube using class name A8_Static
 		System.out.println(result);  
 	}
 }
