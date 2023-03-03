76917059410f246475230c0e22cb26be28b36dcc
MariamShaliniJack31 <mariamshalini@gmail.com>
2023-03-03 18:24:58 -0500

    First Commit on 3rd Mar 23

--- src/com/java/basics/C7SelectDropDown.java
+++ src/com/java/basics/C7SelectDropDown.java
@@ -12,7 +12,7 @@ public class C7SelectDropDown {
 	
 	public static void main(String[] args) throws InterruptedException  {
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\mrufu\\\\Downloads\\\\chromedriver_win32\\\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\mrufu\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS) ;
 		driver.get("http://demo.guru99.com/selenium/newtours/register.php");
@@ -43,19 +43,16 @@ public class C7SelectDropDown {
 		System.out.println(seleceted);
 		
 		List<WebElement> dd = drpCountry.getOptions();
-
 	    //Get the length
 	    System.out.println("The size of the List is : " +dd.size());
-	    // Loop to print one by one
-	    /*for (int j = 0; j < dd.size(); j++) {
-	        System.out.println(dd.get(j).getText());
-	    }*/
 	    
-//	    for(WebElement i : dd)
-//	    {
-//	    	System.out.println(i.getText());
-//	    }
-	    
+	    // Loop to print one by one or ListOption exists or not
+	    for (int j = 0; j < dd.size(); j++) {
+	        //System.out.println(dd.get(j).getText());
+	        if(dd.get(j).getText().contentEquals("INDIA"))
+	    		System.out.println("INDIA exists");
+
+	    }
 	    for(int j = 0; j <= dd.size()-1; j++)
 	    {
 	    	//System.out.println(dd.get(j).getText());
